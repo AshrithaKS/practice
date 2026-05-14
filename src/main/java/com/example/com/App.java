904ebd23ba571f5a68358fad4e41b21b0e34ba54
Ashritha K S <ashrithaks2704@gmail.com>
2026-05-14 07:14:16 +0530

    Update App.java

--- src/main/java/com/example/com/App.java
+++ src/main/java/com/example/com/App.java
@@ -1,7 +1,7 @@
 package com.example.com;
 
-import org.slf4.Logger;
-import org.slf4.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.apache.commons.lang3.StringUtils;
 
 public class App{
